a09170bd4c3da3e09c36713e927fe22af9264e8d
joyns <jelenajovic.ns@gmail.com>
2016-10-26 12:31:45 +0200

    version 11
    replace all for loops with forEach

--- script.js
+++ script.js
@@ -25,25 +25,24 @@ var todoList = {
         var completedTodos = 0;
         
         //Get number of completed todos
-        for (var i=0; i<this.todos.length; i++){
-            if (this.todos[i].completed === true){
-                completedTodos++;
-            }
-        };
+		
+		this.todos.forEach(function(todo){
+			if (todo.completed === true){
+				completedTodos++;
+			}
+		});
         
-        
-        //Case 1: if everything is true, make everything false
-        if (totalTodos === completedTodos){
-            for (var i=0; i < this.todos.length; i++){
-                this.todos[i].completed = false;
-            }
-        }
-        // Case 2: Otherwise, make everything true
-        else {
-           for (var i=0; i<this.todos.length; i++){
-                this.todos[i].completed = true;
-            } 
-        };
+		
+		this.todos.forEach(function(todo){
+			 //Case 1: if everything is true, make everything false
+			if (totalTodos === completedTodos){
+				todo.completed = false;
+			}
+			// Case 2: Otherwise, make everything true
+			else{
+				todo.completed = true;
+			}
+		});
      
     }
 };
@@ -87,9 +86,8 @@ var view = {
 		var todosUl = document.querySelector('ul');
 		todosUl.innerHTML = '';
 		
-		for (var i=0; i < todoList.todos.length; i++){
+		todoList.todos.forEach(function(todo, position){
 			var todoLi = document.createElement('li');
-			var todo = todoList.todos[i];
 			var todoTextWithCompletion = '';
 			
 			if (todo.completed === true) {
@@ -98,11 +96,12 @@ var view = {
 				todoTextWithCompletion = '( ) ' + todo.todoText;
 			}
 			
-			todoLi.id= i;
+			todoLi.id= position;
 			todoLi.textContent = todoTextWithCompletion;
 			todoLi.appendChild(this.createDeleteButton());
 			todosUl.appendChild(todoLi);
-		}
+			
+		}, this);
 		
 	},
 	createDeleteButton: function(){
